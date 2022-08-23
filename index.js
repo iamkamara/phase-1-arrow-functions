3f02319799f069814c788eb3593103f9c6ed008e
onesmus <onesmusqamara@gmail.com>
2022-08-23 10:58:41 +0300

    completed assignment

---
+++ index.js
@@ -0,0 +1,9 @@
+const divide = function (a = 2000, b=100){
+
+    return (a/b);
+}
+console.log(divide(2000, 100));
+
+const square = Number => Number ** 2;
+
+const add = (num1, num2) => num1 + num2;
